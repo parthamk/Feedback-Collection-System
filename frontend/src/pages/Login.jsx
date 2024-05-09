c3352caffbb04b3ecfcfee6121931e7ca3800fea
kaamilnabeel07 <kaamil.nabeel07@gmail.com>
2024-05-09 18:27:11 +0530

    login and signup design changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,52 +1,98 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
-import toast, {Toaster} from 'react-hot-toast';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import Img from "../assets/login.png";
 
 const Login = () => {
   const navigate = useNavigate();
   const [fromData, setFromData] = useState({
     email: "",
     passwordL: "",
-  })
+  });
 
-  const handleChange = (e)=>{
-    setFromData({...fromData, [e.target.name]: e.target.value});
-  }
+  const handleChange = (e) => {
+    setFromData({ ...fromData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/auth/login", fromData);
+      const response = await axios.post(
+        "http://localhost:8000/auth/login",
+        fromData
+      );
       console.log(response.data);
-      localStorage.setItem("adminName", JSON.stringify(response.data));
       toast.success("Logged in successfully");
       navigate("/dashboard");
     } catch (error) {
       toast.error(error.response.data.message);
     }
-  }
+  };
 
   return (
-    <div className='flex items-center justify-center bg-gray-100 min-h-screen'>
-        <Toaster/>
-        <div className='bg-white p-8 rounded shadow-md w-96'>
-            <h2 className='text-2xl font-semibold text-gray-800 mb-6'>Login</h2>
-            <form className='space-y-4' onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="email" className='block text-sm font-medium text-gray-600'>Email</label>
-                <input type="email" name="email" id="email" placeholder='jhondoe@example.com' className='mt-1 w-full outline-none border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-90 block' onChange={handleChange}/>
-              </div>
-              <div>
-                <label htmlFor="password" className='block text-sm font-medium text-gray-600'>Password</label>
-                <input type="password" name="password" id="password" placeholder='password' className='mt-1 w-full outline-none border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-90 block' onChange={handleChange}/>
-              </div>
-              <button type="submit" className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign In</button>
+    <div className="flex items-center justify-center bg-gradient-to-b from-blue-400 to-blue-100 min-h-screen">
+      <Toaster />
+      <div className="w-[900px] h-[600px] bg-white rounded-[20px] flex shadow-2xl">
+        <div className="w-[50%] h-[600px] rounded-[20px] flex justify-center items-center m-4 p-2">
+          <div className="w-[90%] h-[80%] flex flex-col justify-center items-center">
+            <div className="w-[100%] h-[85px] flex justify-start items-start flex-col">
+              <h1 className="font-semibold text-[40px] text-blue-600">
+                Welcome!!
+              </h1>
+              <p className="font-semibold">
+                Not registered yet? <span>
+                <Link to="/register" className="text-blue-600 font-semibold">
+                  Sign up
+                </Link>
+                </span> 
+              </p>
+            </div>
+            <form
+              className="w-[100%] h-[600px] flex justify-around items-center flex-col"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your Email"
+                className="w-[100%] h-[40px] text-start outline-none bg-transparent border-b-2 border-blue-400 shadow-2xl placeholder:text-blue-600 font-semibold"
+                onChange={handleChange}
+              />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter your Password"
+                className="w-[100%] h-[40px] text-start outline-none bg-transparent border-b-2 border-blue-400 shadow-2xl placeholder:text-blue-600 font-semibold"
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                className="w-[70%] h-[40px] bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md shadow-2xl"
+              >
+                Sign In
+              </button>
             </form>
-            <p>Create your account <Link to="/register" className='underline'>Here</Link></p>
+            <p className="text-[16px] font-semibold">
+              Forgotten your password or your login details?
+              <Link to="/register" className="text-blue-600 underline">
+                Get help
+              </Link> signing in
+            </p>
+          </div>
         </div>
+        <div className="w-[50%] h-[600px] flex justify-center items-center rounded-[10px]">
+          <img
+            className="w-[100%] h-[100%] object-cover mix-blend-multiply"
+            src={Img}
+            alt="image"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
