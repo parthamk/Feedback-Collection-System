a24f1342e182eb391a9952689b67147a3b45d23b
parthamk <partha0988@gmail.com>
2024-05-14 02:10:18 +0530

    fixed menue with Link and added _redirects

--- frontend/src/pages/landingpage/Menu.jsx
+++ frontend/src/pages/landingpage/Menu.jsx
@@ -1,30 +1,31 @@
+import { Link } from "react-router-dom"
 
 const Menu = () => {
   return <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
-  <a
-    href="#"
+  <Link
+    to="#"
     className="block md:inline-block px-3 py-2 rounded-md text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
   >
     Home
-  </a>
-  <a
-    href="#about-us"
+  </Link>
+  <Link
+    to="#about-us"
     className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
   >
     About
-  </a>
-  <a
-    href="#contact-us"
+  </Link>
+  <Link
+    to="#contact-us"
     className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
   >
     Contact
-  </a>
-  <a
-    href="/login"
+  </Link>
+  <Link
+    to="/login"
     className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
   >
     Login
-  </a>
+  </Link>
 </div>
 }
 
